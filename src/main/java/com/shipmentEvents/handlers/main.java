48646ee62d5e8ea515fa2a1817145e9776a19ccc
octaviosanti351 <83409019+octaviosanti351@users.noreply.github.com>
2022-01-21 15:10:29 -0300

    Update main.java

--- src/main/java/com/shipmentEvents/handlers/main.java
+++ src/main/java/com/shipmentEvents/handlers/main.java
@@ -1,12 +1,11 @@
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+package com.shipmentEvents.handlers;
+import java.util.Date;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 
-	public static void main(String[] args) {
-		System.out.print("hola");
-	}
 }
+
